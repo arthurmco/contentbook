5ccf02d3dab35b8e2f2b4c29d219a980ad49d1b6
Arthur Mendes <arthurmco@gmail.com>
2015-11-21 01:57:02 -0200

    Added some POST getting code to signup page

--- profile.php
+++ profile.php
@@ -1,5 +1,44 @@
+
+<?php
+
+//Get the variable ID.
+if (!$_GET || !$_GET["id"]){
+	//If it is invalid, use users instead.
+	$userid = 0;
+} else {
+	$userid = $_GET["id"];
+}
+
+?>
 <!DOCTYPE html5>
 
 <!-- Main page of Contentbook -->
 
 <html>
+	<head>
+		<title> %1 - Contentbook </title>
+	</head>
+	
+	<body>
+		<div id="menubar">
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#" id="searcharea"><input type="text" size="20" name="search" /></a></li>
+					<li><a href="#">Setup</a></li>
+					<li><a href="#">Exit</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div id="profile_area">
+			<h1>%1</h1>
+			<div id="user_description"></div>
+			<div id="user_friends">
+				<h1>Friends</h1>
+			</div>
+			<div id="user_posts">
+			</div>
+		</div>
+	<?php include("include/footer.php") ?>
+	</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,3 +1,39 @@
+<?php	
+
+$id = -1;
+$password = 0;
+$password_check = 0;
+if ($_POST && $_POST['id'] > (time(NULL) - 1800)){
+	
+	//Shall be SHA256 instead of SHA1. Need to change this.
+	$password = md5(sha1($_POST['password']));
+	$password_check = md5(sha1($_POST['password_check']));
+	
+	if (($password === $password_check)){
+		//Continue to get other fields.
+		
+		$username = $_POST['username'];
+		$gender = 0;
+		if ($_POST['gender'] === "male"){
+			$gender = 0;
+		} else if ($_POST['gender'] === "female"){
+			$gender = 1;
+		}
+		
+		$birthdate = $_POST['birth-date'];
+		
+	}
+	
+	
+	
+	
+} else if ($_POST && $_POST['id'] < (time(NULL) - 1800)){
+	//You have a 30-min time window to fill the form, for safeness
+	die("Session expired");
+}
+
+?>
+
 <!DOCTYPE html5>
 
 <!-- Sign-up page of Contentbook -->
@@ -6,11 +42,19 @@
 
 <head>
 	<title>Contentbook - Sign-up</title>
+	
 </head>
-
 <body>
 	<h1> Sign up to Contentbook </h1>
-	<form id="signup" method="post" action="signup.php" >
+	<form id="signup" method="post" action="signup.php" onSubmit="signup_submit()">
+		<span id="user_alert">
+			<?php 
+				if (!($password === $password_check)){
+					echo "Passwords do not match!";
+				}
+			?>
+		<span>
+		<input type="hidden" name="id" value="<?php echo time(NULL) ?>" />
 		<p>
 		<label for="username">Username: </label>
 		<input type="text" size="12" name="username" id="username" />
@@ -39,10 +83,12 @@
 		<p> Country:
 			<select name="country" /> </select>
 		<p> City
-			<select name="city"/> </select> </p>
+			<input type="text" name="city"/>  </p>
 		<hr />
 		<input type="submit" value="Sign up" />
 	</form>
+	<?php include("include/footer.php") ?>
+
 </body>
 
 </html>
