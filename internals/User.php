02745a76c56d1c1573c9677402629935dcc725d3
Arthur Mendes <arthurmco@gmail.com>
2016-08-02 22:46:01 -0300

    Started working on Contentbook refactoring
    Removed old code, added some things on User class

--- internals/User.php
+++ internals/User.php
@@ -10,15 +10,17 @@ class User {
     public $sex = '';
     public $birthdate = 0;
     public $formalname = "";
+    public $type;
     
     public $city;
     public $biography;
     
+    private $isFromDatabase = false;
+            
     function __construct($id, $username, $passhash) {
         $this->id = $id;
         $this->$username = $username;
-        $this->passhash = $passhash;
-        
+        $this->passhash = $passhash;       
     }
     /* Returns the user ID */
     public function GetID() { return $this->id; }
@@ -36,6 +38,44 @@ class User {
             self::$LoggedUser = $user;
         }
     }
+    
+    /* Adds the user to the database, or update it if it's added 
+        
+     *  @returns True on success, false on error
+     */
+    public function SaveToDatabase($mysql_link) {    
+        if ($this->isFromDatabase === true) {
+            $query_str = "UPDATE users SET usersex=" . $this->sex . ", ";
+            $query_str .= "userformalname='" . $this->formalname . "', ";
+            $query_str .= "userbirthdate='" . $this->birthdate . "', ";
+            $query_str .= "usertype=" . $this->type . ", ";
+            $query_str .= "usercountry=0, ";
+            $query_str .= "usercity='" . $this->city . "', ";
+            $query_str .= "userautobio='" . $this->biography . "' ";
+            $query_str .= "WHERE userid=" . $this->id;            
+        } else {
+            $query_str = "INSERT INTO users (username, usersex, userpassword, userformalname, ";
+            $query_str .= "userbirthdate, usertype, usercountry, usercity, userautobio";
+            $query_str .= "VALUES ('$this->username',$this->sex,'$this->passhash','$this->formalname', ";
+            $query_str .= "'$this->birthdate',$this->type',0,'$this->city','$this->biography')";            
+        }
+        
+        $result = mysqli_query($mysql_link, $query_str);
+
+        if (!$result){
+            return false;
+        }
+        
+        $last_id = mysql_insert_id($mysql_link);
+        if (!$this->isFromDatabase) {
+            /* Update id */
+            $this->id = $last_id;
+        }
+        return true;
+    }
+        
+    /*** Static functions ***/
+    
     
     /* Get an user from database given its ID */
     public static function GetUserFromID($mysqli_link, $id) {
@@ -63,11 +103,14 @@ class User {
         $showUser->birthdate = $ret_user['userbirthdate'];
         $showUser->city = $ret_user['usercity'];
         $showUser->biography = $ret_user['userautobio'];
+        $showUser->isFromDatabase = true;
          mysqli_free_result($result); 
         return $showUser;
     }
     
-};
+    
+    
+}
 
 
 
